62c9a2dfdaf2e0068591462308bf7aa8c37e3c73
Thiago Campos <commit@thigcampos.com>
2023-11-10 13:19:24 -0300

    install: add debian and arch support

--- fullInstall.sh
+++ fullInstall.sh
@@ -1,3 +1,19 @@
+function get_os() {
+    declare -A osInfo;
+    osInfo[/etc/redhat-release]="dnf install"
+    osInfo[/etc/arch-release]="pacman -S"
+    osInfo[/etc/debian_version]="apt-get install"
+
+    for f in ${!osInfo[@]}
+    do
+        if [[ -f $f ]];then
+            echo ${osInfo[$f]}
+        fi
+    done
+}
+
+PKGMAN=$(get_os)
+
 # Initial
 sudo dnf update -y
 sudo dnf upgrade -y --refresh
@@ -9,27 +25,27 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 echo 'Rust Installed'
 
 # Java
-sudo dnf install java-latest-openjdk-devel.x86_64
+sudo $PKGMAN -y java-latest-openjdk-devel.x86_64
 echo 'Java Installed'
 
 # Rust Commands
-sudo dnf install -y tealdeer procs ripgrep bat fd-find
+sudo $PKGMAN -y tealdeer procs ripgrep bat fd-find
 echo 'All Rust Commmands Installed'
 
 # Fish
-sudo dnf install -y fish
-sudo dnf install -y util-linux-user
+sudo $PKGMAN -y fish
+sudo $PKGMAN -y util-linux-user
 chsh -s /usr/bin/fish
 echo 'Fish Installed'
 
 # VSCodium
 sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
-sudo dnf install -y codium 
+sudo $PKGMAN -y codium 
 echo 'VSCodium Installed'
 
 # Node
-sudo dnf install -y nodejs
+sudo $PKGMAN -y nodejs
 echo 'NodeJS Installed'
 
 # Yarn
@@ -37,26 +53,26 @@ sudo npm install --global yarn
 echo 'Yarn Installed'
 
 # Xclip
-sudo dnf install -y xclip
+sudo $PKGMAN -y xclip
 echo 'Xclip Installed'
 
 # Docker
-sudo dnf -y install dnf-plugins-core
+sudo $PKGMAN -y dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo $PKGMAN -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo 'Docker Installed'
 # Issues? Check: https://docs.docker.com/desktop/install/fedora/
 
 # Neovim
-sudo dnf install -y neovim python3-neovim
+sudo $PKGMAN -y neovim python3-neovim
 echo 'Neovim Installed'
 
 # Neofetch
-sudo dnf install -y neofetch
+sudo $PKGMAN -y neofetch
 echo 'Neofetch Installed'
 
 # RPM Fusion
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo $PKGMAN https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 echo 'RPM Fusion Installed'
 
 # Firefox
